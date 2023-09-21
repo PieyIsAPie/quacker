4fc3282d7e9f14fbb30918b64f3be3fa6884f950
Jude Pearson-Phillips <foreynator.serverowner@gmail.com>
2023-09-21 19:57:39 -0400

    C# now

---
+++ QuackerServer/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace QuackerServer
+{
+    internal class Server
+    {
+        private TcpListener listener;
+
+        public Server() { }
+
+        public void StartServer()
+        {
+            listener = new TcpListener(IPAddress.Any, 65456);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                StreamWriter writer = new StreamWriter(stream, Encoding.ASCII);
+            }
+        }
+    }
+}
--- client/gui.py
+++
@@ -1,9 +0,0 @@
-import Neutron
-from lib import ChatClient
-
-win = Neutron.Window("Quacker Client")
-def connect():
-    win.getElementById("host")
-    win.getElementById("port")
-
-win.getElementById("connect").addEventListener("click", )
